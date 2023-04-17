c8f3f75a9fa4603d2d11d8e04729cc06970d2123
Érica Guimarães <eriquinha_guima@yahoo.com.br>
2023-04-17 17:03:35 -0300

    Requisito 5 - Formulário de pesquisa de artista

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,40 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    artistName: '',
+    btnDisabled: true,
+  };
+
+  onInputChange = ({ target }) => {
+    const { name, value } = target;
+    const tamMinino = 2;
+    this.setState({
+      [name]: value,
+      btnDisabled: value.length < tamMinino,
+    });
+  };
+
   render() {
+    const { artistName, btnDisabled } = this.state;
     return (
       <div data-testid="page-search">
-        Search
         <Header />
+        <form>
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            name="artistName"
+            onChange={ this.onInputChange }
+            value={ artistName }
+          />
+          <button
+            data-testid="search-artist-button"
+            disabled={ btnDisabled }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
