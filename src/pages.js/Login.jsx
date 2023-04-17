a0f54f90b81d870e959707a4cdf69d471d01eb6c
Érica Guimarães <eriquinha_guima@yahoo.com.br>
2023-04-17 13:49:54 -0300

    Requisito 2

--- src/pages.js/Login.jsx
+++ src/pages.js/Login.jsx
@@ -1,11 +1,65 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Carregando from './Carregando';
 
-class Album extends React.Component {
+class Login extends React.Component {
+  state = {
+    nameLogin: '',
+    btnDisable: true,
+    isLoading: false,
+  };
+
+  onInputChange = ({ target }) => {
+    const { name, value } = target;
+    const tamMinino = 3;
+    this.setState({
+      [name]: value,
+      btnDisable: value.length < tamMinino,
+    });
+  };
+
+  handleClick = async () => {
+    const { nameLogin } = this.state;
+    const { history } = this.props;
+    this.setState({ isLoading: true });
+    await createUser({ name: nameLogin });
+    history.push('/search');
+  };
+
   render() {
+    const { nameLogin, btnDisable, isLoading } = this.state;
     return (
-      <div data-testid="page-login">Album</div>
+      <div data-testid="page-login">
+        <h1>Login</h1>
+        { isLoading ? <Carregando /> : (
+          <form>
+            <input
+              data-testid="login-name-input"
+              type="text"
+              value={ nameLogin }
+              name="nameLogin"
+              onChange={ this.onInputChange }
+            />
+            <button
+              data-testid="login-submit-button"
+              disabled={ btnDisable }
+              onClick={ this.handleClick }
+            >
+              Entrar
+            </button>
+          </form>
+        )}
+
+      </div>
     );
   }
 }
 
-export default Album;
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default Login;
