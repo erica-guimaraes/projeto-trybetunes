63eefeab2ae2011dc833230db066535bab4e2da7
Érica Guimarães <eriquinha_guima@yahoo.com.br>
2023-04-21 17:33:51 -0300

    Requisito 7 finalizado

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,15 +1,64 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import getMusics from '../services/musicsAPI';
 
 class Album extends React.Component {
+  state = {
+    musics: [],
+    artistName: '',
+    albumName: '',
+  };
+
+  componentDidMount() {
+    this.handleMusic();
+  }
+
+  handleMusic = async () => {
+    const { match: { params: { id } } } = this.props;
+    let musicApi = await getMusics(id);
+    musicApi = musicApi.filter((music) => music.trackName);
+    // console.log(musicApi);
+    // const musicas = musicApi.filter((music) => music.wrapperType === track);
+    // console.log(musicApi);
+    this.setState({
+      musics: musicApi,
+      artistName: musicApi[0].artistName,
+      albumName: musicApi[0].collectionName,
+    });
+  };
+
   render() {
+    const { musics, artistName, albumName } = this.state;
     return (
       <div data-testid="page-album">
         Album
         <Header />
+        <div>
+          <h4 data-testid="album-name">{ albumName }</h4>
+          <p data-testid="artist-name">{ artistName }</p>
+        </div>
+        <div>
+          {musics.map((music, index) => (
+            <MusicCard
+              key={ index }
+              { ...music }
+            />
+          ))}
+        </div>
+
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default Album;
